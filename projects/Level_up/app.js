82a8a9f23b4588e1f32570aa10cc69f185dc0da0
Colton Anderson <anderson0741@icloud.com>
2017-12-19 18:44:26 -0700

    update

--- projects/Level_up/app.js
+++ projects/Level_up/app.js
@@ -1,10 +1,16 @@
 let vid = document.getElementById("intro_video"); 
-
+vid.volume = 0.09;
+var doc = aEvent.originalTarget
 function playVid() { 
     vid.play(); 
 } 
-
 function pauseVid() { 
     vid.pause(); 
 }
 
+// video.addEventListener('loadeddata', function(){
+//     video.play();
+// });
+// video.addEventListener('pause', function(){
+//     video.play();
+// })
