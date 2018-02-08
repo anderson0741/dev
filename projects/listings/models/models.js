0b8192839d1da8048090eefd69752bb3b6084e42
Lawrence Anderson <anderson0741@icloud.com>
2018-02-08 11:43:51 -0700

    react-app-website

--- projects/listings/models/models.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const listingSchema = new mongoose.Schema({
-
-});
-
-module.exports = mongoose.model("Listings", listingSchema);
