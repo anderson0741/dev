4866049486b57ddb6123f62ece17336ea26a3bd8
Lawrence Anderson <anderson0741@icloud.com>
2018-02-26 13:35:41 -0700

    sidebars

--- Clients/mary_p_a/client/src/App/index.js
+++ Clients/mary_p_a/client/src/App/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import { Switch, Route } from "react-router-dom";
 import Nav from '../Nav/index';
+import LeftSideBar from '../SideBars/LeftSideBar';
+import RightSideBar from '../SideBars/RightSideBar';
+import Home from '../shared/Home';
+
+import './index.css';
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                <Nav />
+                <div className="nav">
+                    <Nav />
+                </div>
+                <br />
+                <div className="content">
+                    <LeftSideBar />
+                    <Switch>
+                        <Route path="/home" component={Home} />
+                    </Switch>
+                    <RightSideBar />
+                </div>
             </div>
         )
     }
--- Clients/mary_p_a/client/src/Nav/index.js
+++ Clients/mary_p_a/client/src/Nav/index.js
@@ -1,35 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 // import images from '../../images/level_up_cars.jpg';
-// import Home from '../../shared/Home/index.js';
+import Home from '../shared/Home';
+import { connect } from "react-redux";
+import { logout } from "../redux/auth";
 
 
 import './Nav.css';
 
-function Nav() {
-    return (
-        <div className="papa">
-            <div className="navBod">
-                {/* <img className='imgs' src={images} alt="" /> */}
-                <div className="navi ">
-                    {/* <img className='imgs' src={images} alt="" /> */}
-                    <p className='links'>/</p>
-                    <Link className='links' to="/">Home</Link>
-                    <p className='links'>/</p>
-                    <Link className='links' to="/about">About</Link>
-                    <p className='links'>/</p>
-                    <Link className='links' to="/cars">Shop Cars</Link>
-                    <p className='links'>/</p>
-                    <Link className='links' to="/upload">Upload Content</Link>
-                    <p className='links'>/</p>
-                    {/* <Link className='links' to="/image">Upload Image</Link>
-                    <p className='links'>/</p> */}
-                    <Link className='links' to="/contact">Contact Us</Link>
-                    <p className='links'>/</p>
+class Nav extends Component {
+    render() {
+        const isAuthenticated = this.props.isAuthenticated;
+        return (
+            <div className="papa">
+                <div className="navBod">
+                    <div className="navi ">
+                        <div><p className='links'>/</p></div>
+                        {isAuthenticated ? null : <div><Link className='links' to="/">Home</Link></div>}
+                        <div><p className='links'>/</p></div>
+                        {isAuthenticated ? null : <div><Link className='links' to="/about">About</Link></div>}
+                        <div><p className='links'>/</p></div>
+                        {isAuthenticated ? <div><Link className='links' to="/upload">Upload Posts</Link></div> : null}
+                        <div><p className='links'>/</p></div>
+                        {isAuthenticated ? null : <div><Link className='links' to="/contact">Contact</Link></div>}
+                        <div><p className='links'>/</p></div>
+                        {isAuthenticated ? null : <div className="navlinkz"><Link to="/login">Login</Link></div>}
+                        {isAuthenticated ? <div className="navlinkz">
+                            <Link onClick={this.props.logout}>Logout</Link>
+                        </div> : null}
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Nav;
