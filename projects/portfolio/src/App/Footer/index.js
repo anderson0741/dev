4f5eda1f4881c4c8d6dcd686f5689d0a4b5616d0
Lawrence Anderson <anderson0741@icloud.com>
2018-03-14 13:32:39 -0600

    footer

---
+++ projects/portfolio/src/App/Footer/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import LinkedIn from '../icons/linkedin2.png';
+import GitHub from '../icons/github.png';
+
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className="footer_parent">
+            <div className="linkedIn">
+                <a href="https://www.linkedin.com/in/anderson0741/"><img className="icons" src={LinkedIn}/></a>
+            </div>
+            <div className="gitHub">
+                <a href="https://github.com/anderson0741"><img className="icons" src={GitHub}/></a>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
--- projects/portfolio/src/App/index.js
+++ projects/portfolio/src/App/index.js
@@ -13,26 +13,18 @@ import Level_Up_Login from './level_up/Login_Level';
 import Level_Up_Content from './level_up/Content/Content';
 import Starwars_API from './StarwarsAPI';
 import Character from './StarwarsAPI/Sidebar/Character';
+import Footer from './Footer';
 
 import './App.css'
 
 class App extends Component {
-    // choosePic() {
-    //     let { pathname } = this.props.location;
-    //     switch (pathname) {
-    //         case "/starwarsapi":
-    //             return "http://orig12.deviantart.net/53f9/f/2016/338/7/d/star_wars___death_star_wallpaper_by_rocklou-daqj3gl.png";
 
-    //         default:
-    //             return ""
-    //     }
-    // }
     render() {
         console.log(this.props.location.pathname)
         // const globalStyles = { backgroundImage: `url(${this.choosePic()})` }
         return (
             <div>
-            {/* <div className="global_div" style={globalStyles}> */}
+                {/* <div className="global_div" style={globalStyles}> */}
                 <Nav />
                 <Switch>
                     <Route exact path='/' component={Home} />
@@ -46,6 +38,7 @@ class App extends Component {
                     <Route path='/starwarsapi' component={Starwars_API} />
                     <Route path='/characters/:id' component={Character} />
                 </Switch>
+                <Footer />
             </div>
         )
     }
