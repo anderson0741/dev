e7d2f33cb6f72eee523a8fcd253e72885415ae1a
Lawrence Anderson <anderson0741@icloud.com>
2018-03-14 20:47:55 -0600

    mobile portfolio

--- assignments-before-react/exercises/functions/function.js
+++ assignments-before-react/exercises/functions/function.js
@@ -10,23 +10,23 @@ function questionTwo (num1, num2, num3) {
 
 console.log(questionTwo(10, 9, 3));
 
-// var questionThree = function(number) {
-//     // Your code goes here!
-//     if (((number/2) - Math.floor(number/2)) === 0) {
-//         return "even";
-//         } else {
-//             return "odd";
-//         };
-//   }; 
-//   console.log(questionThree(6));
+var questionThree = function(number) {
+    // Your code goes here!
+    if (((number/2) - Math.floor(number/2)) === 0) {
+        return "even";
+        } else {
+            return "odd";
+        };
+  }; 
+  console.log(questionThree(6));
 
-// var str = "juan went on a trip in his favorite rocket ship";
-// var questionFour = function() {
-//     if (str.length < 20){
-//         return str + str;
-//     } else { 
-//         return str.slice(0, str.length/2);
-//     }
-// }
-//     console.log(questionFour());
+var str = "juan went on a trip in his favorite rocket ship";
+var questionFour = function() {
+    if (str.length < 20){
+        return str + str;
+    } else { 
+        return str.slice(0, str.length/2);
+    }
+}
+    console.log(questionFour());
 
