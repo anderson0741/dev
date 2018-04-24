c8edb1b56eb6cca626b6d415141e7b3ab66f18e4
Lawrence Anderson <anderson0741@icloud.com>
2018-04-24 17:10:33 -0600

    server fix

--- Websites/utah-european-doberman/client/src/shared/Contact/index.js
+++ Websites/utah-european-doberman/client/src/shared/Contact/index.js
@@ -4,12 +4,11 @@ import './Contact.css';
 function Contact() {
     return (
         <div class="addresses">
-                    <div className="surveyTitle">
-                        <h1 className='detailSurvey'>Reserve your European Doberman</h1>
-                        <p className='detailSurvey'>Dogs are 2,000 - 3,200 based on options selected. </p>
-                        <h3 className='detailSurvey'>Email: Europeandobes@gmail.com</h3>
-                    </div>
-            {/* <button className="submit" onClick={this.props.toggleEdit}>Cancel</button> */}
+            <div className="surveyTitle">
+                <h1 className='detailSurvey'>Reserve your European Doberman</h1>
+                <p className='detailSurvey'>Dogs are 2,000 - 3,200 based on options selected. </p>
+                <h3 className='detailSurvey'>Email: Europeandobes@gmail.com</h3>
+            </div>
         </div>
     )
 }
